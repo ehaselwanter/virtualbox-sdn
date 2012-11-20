b48d7a130ca766a547f2aac17c21cc9dd41b1ba5
Edmund Haselwanter <edmund@haselwanter.com>
2012-11-20 09:44:18 +0100

    checkout the trema repositories

--- vendor/cookbooks/trema/recipes/default.rb
+++ vendor/cookbooks/trema/recipes/default.rb
@@ -7,9 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w{ruby ruby-dev rubygems git libpcap-dev libsqlite3-dev}.each do |package_name|
+%w{ruby ruby-dev rubygems git libpcap-dev libsqlite3-dev tmux vim}.each do |package_name|
 	package package_name
 end
 
 gem_package "trema"
+
+git "/home/vagrant/trema" do                            
+    repository "https://github.com/trema/trema.git"
+    revision "master"                                   
+    action :sync                                     
+    user "vagrant"                                    
+    group "vagrant"  
+end
+
+git "/home/vagrant/trema-apps" do                            
+    repository "https://github.com/trema/apps.git"
+    revision "master"                                   
+    action :sync                                     
+    user "vagrant"                                    
+    group "vagrant"  
+end
+
 
